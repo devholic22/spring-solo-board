121dc54cc8244a3169081614505423e493292a7c
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-01-10 15:08:33 +0900

    ✅ test: 테스트 차 데이터 추가 등록
    - local profile을 사용하도록 설정
    - 테스트 데이터를 등록해두는 TestInit 파일 추가
    - local profile 사용 시 TestInit을 실행하도록 설정

--- src/main/java/devholic22/board/BoardApplication.java
+++ src/main/java/devholic22/board/BoardApplication.java
@@ -1,9 +1,12 @@
 package devholic22.board;
 
 import devholic22.board.config.MemoryConfig;
+import devholic22.board.repository.BoardRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Import(MemoryConfig.class)
@@ -13,4 +16,9 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Bean
+	@Profile("local")
+	public TestInit testInit(BoardRepository boardRepository) {
+		return new TestInit(boardRepository);
+	}
 }
---
+++ src/main/java/devholic22/board/TestInit.java
@@ -0,0 +1,23 @@
+package devholic22.board;
+
+import devholic22.board.domain.Board;
+import devholic22.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TestInit {
+
+    private final BoardRepository boardRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        log.info("test data init");
+        boardRepository.save(new Board("anon", "test board 1", "this is test 1"));
+        boardRepository.save(new Board("anon", "test board 2", "this is test 2"));
+        boardRepository.save(new Board("anon", "test board 3", "this is test 3"));
+    }
+}
