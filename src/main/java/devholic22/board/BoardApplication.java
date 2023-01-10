11c488e0174e5331afc634b2ffb03bfc6aac4bd6
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-01-10 15:06:10 +0900

    🔧 config: MemoryConfig 사용
    - MemoryConfig 설정 파일을 사용하도록 설정

--- src/main/java/devholic22/board/BoardApplication.java
+++ src/main/java/devholic22/board/BoardApplication.java
@@ -1,9 +1,12 @@
 package devholic22.board;
 
+import devholic22.board.config.MemoryConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(MemoryConfig.class)
 public class BoardApplication {
 
 	public static void main(String[] args) {
