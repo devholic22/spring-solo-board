d52d1ff0697d49609cefc4fb96543b3fe675ef0b
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-01-12 21:50:07 +0900

    ⚡️ update: 컨트롤러 코드 수정
    - 서비스에 따른 컨트롤러  코드 수정
    - 잘못된 쿼리 파라미터 접근 시 루트 페이지로 리다이렉트 되도록 설정
    - 검색 로직 구조 개선

--- src/main/java/devholic22/board/controller/MainController.java
+++ src/main/java/devholic22/board/controller/MainController.java
@@ -1,7 +1,7 @@
 package devholic22.board.controller;
 
 import devholic22.board.domain.Board;
-import devholic22.board.repository.dto.SearchDto;
+import devholic22.board.repository.dto.SearchCond;
 import devholic22.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
 
@@ -22,22 +21,34 @@ public class MainController {
 
     // 검색 조회 (필터링)
     @GetMapping()
-    public String Search(@RequestParam(required = false) String search, @RequestParam(defaultValue = "1") Integer page, @ModelAttribute SearchDto searchDto, Model model) {
+    public String search (@ModelAttribute SearchCond searchCond, Model model) {
 
-        Map<Long, Board> allBoard = null;
+        Map<Integer, Board> allBoard;
 
-        if (search != null && searchDto != null) {
-            allBoard = boardService.search(page, searchDto);
-        } else {
-            allBoard = boardService.findAll(page);
+        String search = searchCond.getSearch() != null ? searchCond.getSearch() : "";
+        int page = searchCond.getPage() != null ? searchCond.getPage() : 1;
+        int EACH = 3;
+        int MAX_PAGE;
+        int ALL_SIZE;
+
+        allBoard = boardService.search(searchCond);
+        ALL_SIZE = boardService.findByTitleCount(search);
+        MAX_PAGE = ALL_SIZE % EACH == 0 ? ALL_SIZE / EACH : ALL_SIZE / EACH + 1;
+
+        if (page <= 0 || page > MAX_PAGE) {
+            return "redirect:/";
         }
 
-        Collection<Board> boardValues = allBoard == null ? null : allBoard.values();
-        List<Board> boards = allBoard == null ? null : new ArrayList<>(boardValues);
+        Collection<Board> boardValues = allBoard.values();
+        List<Board> boards = new ArrayList<>(boardValues);
+
+        if (boards.size() == 0) {
+            return "redirect:/";
+        }
 
         model.addAttribute("boards", boards);
         model.addAttribute("page", page);
-
+        model.addAttribute("max_page", MAX_PAGE);
         return "home";
     }
 }
